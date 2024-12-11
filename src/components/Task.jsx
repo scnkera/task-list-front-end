689972ee0996d1fab68763061b0c99afab959d58
Christelle Nkera <scnkera@gmail.com>
2024-12-11 14:59:11 -0500

    Commented out wave-01 implementation

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -20,6 +20,17 @@ const Task = ({ id, title, isComplete }) => {
   );
 };
 
+// Wave-01 implementation
+
+// const Task = ({ id, title, isComplete }) => {
+//   return (
+//     <li className="tasks__item">
+//       <button className="tasks__item__toggle">{title}</button>
+//       <button className="tasks__item__remove button">x</button>
+//     </li>
+//   );
+// };
+
 Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
