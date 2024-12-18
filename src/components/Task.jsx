f3c94184f7340f177a0c9ec1b353659d3f3fabb0
Christelle Nkera <scnkera@gmail.com>
2024-12-18 14:04:35 -0500

    Updated TaskList and Task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,38 +1,27 @@
 import PropTypes from 'prop-types';
 import './Task.css';
 
-const Task = ({ id, title, isComplete, onToggleComplete, onDeleteTask}) => {
+const Task = ({ id, title, isComplete, onClickCallback, onDeleteCallback }) => {
   return (
     <li className="tasks__item">
       <button
         className="tasks__item__toggle"
-        onClick={() => onToggleComplete(id)}
+        onClick={() => onClickCallback(id)}
       >
         {isComplete ? '✔️ ' : '⬜ '}
-        {title}
       </button>
-      <button className="tasks__item__remove button" onClick={() => onDeleteTask(id)}>x</button>
+      <span>{title}</span> {/* Title is now separate */}
+      <button className="tasks__item__remove button" onClick={() => onDeleteCallback(id)}>x</button>
     </li>
   );
 };
-
-// Wave-01 implementation
-
-// const Task = ({ id, title, isComplete }) => {
-//   return (
-//     <li className="tasks__item">
-//       <button className="tasks__item__toggle">{title}</button>
-//       <button className="tasks__item__remove button">x</button>
-//     </li>
-//   );
-// };
 
 Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
-  onToggleComplete: PropTypes.func.isRequired,
-  onDeleteTask: PropTypes.func.isRequired,
+  onClickCallback: PropTypes.func.isRequired,
+  onDeleteCallback: PropTypes.func.isRequired,
 };
 
 export default Task;
